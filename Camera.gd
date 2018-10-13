b22fc694c888132e1ff907bca36aec1690e3d83e
Patrick Werner <werninator@gmail.com>
2018-10-13 14:50:01 +0200

    Visual Improvements for the Player

--- Camera.gd
+++ Camera.gd
@@ -1,12 +1,14 @@
 extends Camera2D
 
+const FOLLOW_NODE_GROUP = "Player"
+
 var follow_node
 
 func _ready():
-	var player_node = get_tree().get_nodes_in_group("Player")
+	var nodes_in_group = get_tree().get_nodes_in_group(FOLLOW_NODE_GROUP)
 	
-	if player_node.size():
-		follow_node = player_node.pop_back()
+	if nodes_in_group.size():
+		follow_node = nodes_in_group.pop_back()
 
 func _process(delta):
 	position = follow_node.position
--- Player.gd
+++ Player.gd
@@ -1,22 +1,34 @@
 extends KinematicBody2D
 
-const MOVEMENT_SPEED = 12
+const MOVEMENT_SPEED = 10
+var initial_x_scale
 
 func _init():
-	pass
+	print($Sprite)
 
 func _process(delta):
+	if !initial_x_scale:
+		initial_x_scale = $Sprite.scale.x
 	handle_movement(delta)
 
 func handle_movement(delta):
 	var input_vector = get_input_vector()
 
 	if input_vector != Vector2():
+		if input_vector.x:
+			flip(input_vector.x)
+
 		move_and_slide(
 			input_vector.normalized() *
 			MOVEMENT_SPEED * 1000 *
 			delta
 		)
+
+		if $AnimationPlayer.current_animation != "Walk":
+			$AnimationPlayer.play("Walk")
+	else:
+		if $AnimationPlayer.current_animation != "RESET":
+			$AnimationPlayer.play("RESET")
 
 func get_input_vector():
 	var input_vector = Vector2()
@@ -31,3 +43,12 @@ func get_input_vector():
 		input_vector.x -= 1
 	
 	return input_vector
+
+func flip(x):
+	var factor = sign(x)
+
+	if $Sprite.scale.x == factor * initial_x_scale || $Tween.is_active():
+		return
+
+	$Tween.interpolate_property($Sprite, "scale:x", $Sprite.scale.x, initial_x_scale * factor, .1, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	$Tween.start()
